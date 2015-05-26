0fc22dfa1e95716e95967e86b62b01ced8dbf6ec
Chris Pennello <git@chrispennello.com>
2015-05-26 07:15:12 -0700

    Writer.

--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@ TODO
 ----
 - write godoc documentation
 - maybe generate masks.go from something easier to read?
+- test writer
--- write.go
+++ write.go
@@ -2,6 +2,11 @@
 
 package steg
 
+import (
+	"errors"
+	"io"
+)
+
 func (c chunk) write(b byte) {
 	cur := c.read()
 	// Compare current value with what we need to write.
@@ -15,4 +20,50 @@ func (c chunk) write(b byte) {
 	// Flip the bit.
 	c[byteIndex] ^= mask
 	// Done!
+}
+
+var ErrShortRead = errors.New("insufficient piggy-back data to write requested data")
+
+type Writer struct {
+	w   io.Writer
+	pig io.Reader
+}
+
+func NewWriter(w io.Writer, pig io.Reader) Writer {
+	return Writer{w: w, pig: pig}
+}
+
+func (w Writer) write(buf []byte, b byte) error {
+	nt := 0
+	for {
+		n, err := w.pig.Read(buf[nt:])
+		nt += n
+		if nt == len(buf) {
+			if err != nil && err != io.EOF {
+				return err
+			}
+			break
+		}
+		if err != nil {
+			if err == io.EOF {
+				return ErrShortRead
+			} else {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (w Writer) Write(p []byte) (int, error) {
+	buf := make([]byte, chunkSize)
+	n := 0
+	for _, b := range p {
+		err := w.write(buf, b)
+		if err != nil {
+			return n, err
+		}
+		n++
+	}
+	return n, nil
 }
