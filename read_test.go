ab138e4149252cf46e60ff86a245aac8062c44bc
Chris Pennello <git@chrispennello.com>
2015-05-26 19:50:34 -0700

    Minor improvement to read test.

--- read_test.go
+++ read_test.go
@@ -5,8 +5,10 @@ package steg
 import "testing"
 
 func TestReadByte(t *testing.T) {
-	testReadByte(t, chunk("hello, there, how are you? fine."))
-	//for i := bitIndex(0); i < 8; i++ {
-	//	testReadByte(t, chunk(masksByIndex[i]))
-	//}
+	testReadByte(t, testHelloChunk())
+	for i := bitIndex(0); i < 8; i++ {
+		if testReadByte(t, chunk(masksByIndex[i])) != 0 {
+			t.Errorf("mask at index %v didn't yield 0", i)
+		}
+	}
 }
