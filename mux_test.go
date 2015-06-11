783f980e463ed41e2505c43c8c56d8f5aefb5962
Chris Pennello <git@chrispennello.com>
2015-06-10 20:34:23 -0700

    Adds Mux padding test.
    Forgot to add this earlier.

---
+++ mux_test.go
@@ -0,0 +1,41 @@
+// chris 061015
+
+package steg
+
+import (
+	"bytes"
+	"testing"
+
+	cryptorand "crypto/rand"
+	mathrand "math/rand"
+)
+
+func testMuxPad(t *testing.T, atomSize uint) {
+	ctx := NewCtx(atomSize)
+
+	msgLen := mathrand.Intn(4) + 1
+	msgBytes := make([]byte, msgLen)
+	if _, err := cryptorand.Read(msgBytes); err != nil {
+		t.Error(err)
+	}
+	msg := bytes.NewBuffer(msgBytes)
+	carrierLen := uint(msgLen) / ctx.atomSize * ctx.chunkSize
+	carrierBytes := make([]byte, carrierLen)
+	if _, err := cryptorand.Read(carrierBytes); err != nil {
+		t.Error(err)
+	}
+	carrier := bytes.NewBuffer(carrierBytes)
+
+	dst := new(bytes.Buffer)
+	m := ctx.NewMux(dst, carrier, msg)
+	if err := m.Mux(); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestMuxPad(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		testMuxPad(t, 1)
+		testMuxPad(t, 2)
+	}
+}
